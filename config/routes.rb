2c1b7800db54324970790d8f56537fe227970a0a
EM-printmaker <esashika191008@outlook.com>
2026-01-20 17:40:03 +0000

    feat: #2 画像配信のURLを変更

--- config/routes.rb
+++ config/routes.rb
@@ -12,28 +12,13 @@ Rails.application.routes.draw do
   root "top#index"
 
   direct :cdn_image do |model, options|
-    expires_in = options.delete(:expires_in) { ActiveStorage.urls_expire_in }
-    host_option = ENV["CDN_HOST"].present? ? { host: ENV["CDN_HOST"], protocol: :https } : {}
+    target = model.respond_to?(:processed) ? model.processed : model
+    key = target.respond_to?(:key) ? target.key : (target.respond_to?(:blob) ? target.blob.key : nil)
 
-    if model.respond_to?(:signed_id)
-      route_for(
-        :rails_service_blob_proxy,
-        model.signed_id(expires_in: expires_in),
-        model.filename,
-        options.merge(host_option)
-      )
+    if key.present? && ENV["CDN_HOST"].present?
+      "https://#{ENV["CDN_HOST"]}/#{key}"
     else
-      signed_blob_id = model.blob.signed_id(expires_in: expires_in)
-      variation_key  = model.variation.key
-      filename       = model.blob.filename
-
-      route_for(
-        :rails_blob_representation_proxy,
-        signed_blob_id,
-        variation_key,
-        filename,
-        options.merge(host_option)
-      )
+      route_for(:rails_blob, model, options)
     end
   end
 end
