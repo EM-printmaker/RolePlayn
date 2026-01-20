c3791ef61b04a1633ae6daf4505c1674b45724cd
EM-printmaker <esashika191008@outlook.com>
2026-01-20 10:39:29 +0000

    debug: #2 画像がCloudFront経由で表示されるか確認のためのviewを追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,6 @@
 class TopController < ApplicationController
   def index
+    # これは表示確認用の仮の変数です。
+    @worlds = World.includes(cities: { characters: { expressions: { image_attachment: { blob: :variant_records } } } }).all
   end
 end
