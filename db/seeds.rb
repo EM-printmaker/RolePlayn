4127502202227d423d9e6e76c2f72b01fce85672
EM-printmaker <esashika191008@outlook.com>
2026-01-20 10:22:52 +0000

    add: #2 テスト画像の追加とseedにテストデータを実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+world = World.find_or_create_by!(name: "ヴェルストリア")
+
+city = world.cities.find_or_create_by!(name: "ルミノア")
+
+characters_data = [
+  { name: "村人", emotion_type: :joy },
+  { name: "騎士", emotion_type: :fun }
+]
+
+characters_data.each_with_index do |data, i|
+  character = city.characters.find_or_create_by!(name: data[:name]) do |c|
+    c.description = "これはキャラクター#{data[:name]}のテスト文章です。"
+  end
+
+  (1..2).each do |lvl|
+    filename = "character_test_image#{i + 1}-#{lvl}.png"
+    image_path = Rails.root.join('spec', 'fixtures', filename)
+
+    expression = character.expressions.find_or_create_by!(
+      emotion_type: data[:emotion_type],
+      level: lvl
+    )
+    expression.image.attach(
+      io: image_path.open,
+      filename: filename,
+      content_type: 'image/png'
+    )
+  end
+end
