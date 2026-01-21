8b45b1b1089f3d8a323d6498879fb8bb84137b54
EM-printmaker <esashika191008@outlook.com>
2026-01-21 11:03:21 +0000

    feat: #3 キャラクター抽選のメソッドを切り分け、再抽選メソッドを追加

--- app/controllers/concerns/character_session_manageable.rb
+++ app/controllers/concerns/character_session_manageable.rb
@@ -1,3 +1,4 @@
+# ユーザーごとのキャラクター抽選をセッションで保持・管理する。
 module CharacterSessionManageable
   extend ActiveSupport::Concern
 
@@ -6,21 +7,22 @@ module CharacterSessionManageable
   end
 
   def set_active_character(city)
-    today = Time.zone.today.to_s
-    reset_character_session if session[:assigned_date] != today
+    # 0時を回ったタイミングでセッションを切り替える仕様のため
+    reset_character_session if session[:assigned_date] != Time.zone.today.to_s
 
     return if session[:active_character_id].present?
+    return if city.blank?
 
-    character = city.characters.pick_random
-    if character
-      expression = character.expressions.pick_random
-      session[:active_character_id] = character.id
-      session[:active_expression_id] = expression&.id
-      session[:assigned_date] = today
-    end
+    character_shuffle(city)
+  end
+
+  def refresh_character(city)
+    reset_character_session
+    character_shuffle(city)
   end
 
   def current_character
+    # find_byがnilを返すケース（キャラ削除時など）でもDB再検索を防ぐためdefined?を使用
     return @current_character if defined?(@current_character)
     @current_character = Character.find_by(id: session[:active_character_id])
   end
@@ -32,10 +34,22 @@ module CharacterSessionManageable
 
   private
 
+    def character_shuffle(city)
+      character = city.characters.pick_random
+      if character
+        expression = character.expressions.pick_random
+        session[:active_character_id] = character.id
+        session[:active_expression_id] = expression&.id
+        session[:assigned_date] = Time.zone.today.to_s
+      end
+    end
+
     def reset_character_session
       session.delete(:active_character_id)
       session.delete(:active_expression_id)
       session.delete(:assigned_date)
+
+      # defined?によるキャッシュを無効化するため、物理的に変数を削除する必要がある
       remove_instance_variable(:@current_character) if defined?(@current_character)
       remove_instance_variable(:@current_expression) if defined?(@current_expression)
     end
