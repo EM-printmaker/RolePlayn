4bbe6429f1c1c41784f54136fe20ec1212342a67
EM-printmaker <esashika191008@outlook.com>
2026-01-27 08:20:26 +0000

    add: #4 WorldにURL生成用のSlugカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_27_060646) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_27_073506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,6 +93,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_27_060646) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_global", default: false, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_worlds_on_slug", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
