e6e55fe60e1524ed9e57fcb8b663dfafe5d1eec7
EM-printmaker <esashika191008@outlook.com>
2026-01-31 17:44:12 +0000

    test: #4 rspec用事前データの更新

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,22 @@
 FactoryBot.define do
   factory :post do
     sequence(:content) { |n| "MyText #{n}" }
-    character
-    expression
     city
+    character { association :character, city: city }
+    expression { association :expression, character: character }
+    sequence(:sender_session_token) { |n| "session_token_#{n}" }
+
+    transient do
+      parent_world { nil }
+      parent_city { nil }
+      given_character { nil }
+      given_expression { nil }
+    end
+
+    trait :with_full_data do
+      city { parent_city || (parent_world ? association(:city, world: parent_world) : association(:city)) }
+      character { given_character || association(:character, city: city) }
+      expression { given_expression || association(:expression, :with_image, character: character) }
+    end
   end
 end
