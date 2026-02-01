a00d97925c75d785959a2c685108b44b7ebf1cbf
EM-printmaker <esashika191008@outlook.com>
2026-02-01 14:39:15 +0000

    feat: #5 login_idにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :trackable
+
+  enum :role, { general: 0, admin: 10 }
+
+  validates :login_id,
+            uniqueness: { case_sensitive: false },
+            format: { with: /\A[a-z0-9_]+\z/i },
+            length: { in: 3..20 },
+            allow_blank: true
+
+  before_validation :downcase_login_id
+
+  private
+
+  def downcase_login_id
+    self.login_id = login_id.downcase if login_id.present?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_01_081140) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_01_091134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -107,8 +107,11 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_01_081140) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "login_id"
+    t.integer "role", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["login_id"], name: "index_users_on_login_id", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
