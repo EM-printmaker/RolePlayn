f2e20b301aaa3058fd412e43935d7c7aae11e569
EM-printmaker <esashika191008@outlook.com>
2026-02-02 18:11:22 +0000

    test: #21 CharacterSessionManageable変更に合わせたRSpecの更新

--- spec/requests/operations/re_rolls_spec.rb
+++ spec/requests/operations/re_rolls_spec.rb
@@ -10,19 +10,43 @@ RSpec.describe "Operations::ReRolls", type: :request do
     let(:new_character) { create(:character, city: city) }
     let(:new_expression) { create(:expression, :with_image,  character: new_character) }
 
+    def current_guest_assignment
+      session.dig(:guest_assignments, city.id.to_s)
+    end
+
     before do
       allow(Expression).to receive(:pick_random).and_return(expression, new_expression)
       get root_path
     end
 
     it "セッションに保存されるキャラクターの表情が更新されること" do
-      previous_expression_id = session[:active_expression_id]
-      post re_rolls_path
-      expect(response).to have_http_status(:see_other)
-      expect(response).to redirect_to(root_path(format: :html))
+      initial_assignment = current_guest_assignment
+      initial_character_id = initial_assignment["character_id"]
 
-      expect(session[:active_expression_id]).not_to eq(previous_expression_id)
-      expect(session[:active_expression_id]).to be_present
+        post re_rolls_path(city_id: city.id),
+          headers: { "HTTP_REFERER" => world_city_path(world, city) }
+
+        new_assignment = current_guest_assignment
+        expect(new_assignment["character_id"]).not_to eq(initial_character_id)
+        expect(new_assignment["character_id"]).to be_present
+        expect(new_assignment["expression_id"]).to be_present
+    end
+
+    context "ログインユーザーの場合" do
+      let(:user) { create(:user) }
+
+      before { sign_in user }
+
+      it "データベース(CharacterAssignment)のレコードが更新されること" do
+        get world_city_path(world, city)
+        initial_assignment = CharacterAssignment.find_by(user: user, city: city)
+        initial_character_id = initial_assignment.character_id
+
+        post re_rolls_path(city_id: city.id)
+
+        updated_assignment = initial_assignment.reload
+        expect(updated_assignment.character_id).not_to eq(initial_character_id)
+      end
     end
   end
 end
--- spec/requests/operations/shuffles_spec.rb
+++ spec/requests/operations/shuffles_spec.rb
@@ -12,21 +12,39 @@ RSpec.describe "Operations::Shuffles", type: :request do
     end
 
     it "セッションに保存される街が更新されること" do
-      expect { post shuffles_path }.to(change { session[:viewing_city_id] })
+      initial_city_id = session[:viewing_city_id]
+      post shuffles_path
+      expect(session[:viewing_city_id]).not_to eq(initial_city_id)
       expect(session[:viewing_city_id]).to be_present
     end
 
     it "セッションに保存されるキャラクターが更新されること" do
-      expect { post shuffles_path }.to(change { session[:active_character_id] })
-      expect(session[:active_character_id]).to be_present
+      post shuffles_path
+      new_city_id = session[:viewing_city_id]
+      assignment = session.dig(:guest_assignments, new_city_id.to_s)
+      expect(assignment).to be_present
+      expect(assignment["character_id"]).to be_present
     end
 
     it "更新した街にリダイレクトされること" do
-      current_city_id = session[:viewing_city_id]
-      new_city = cities.find { |c| c.id != current_city_id }
       post shuffles_path
+      new_city = City.find(session[:viewing_city_id])
       expect(response).to have_http_status(:see_other)
       expect(response).to redirect_to(city_path(new_city))
+    end
+
+    context "ログインユーザーの場合" do
+      let(:user) { create(:user) }
+
+      before { sign_in user }
+
+      it "移動先の街の CharacterAssignment レコードが作成されること" do
+        post shuffles_path
+        new_city_id = session[:viewing_city_id]
+
+        assignment = CharacterAssignment.find_by(user: user, city_id: new_city_id)
+        expect(assignment).to be_present
+      end
     end
   end
 end
