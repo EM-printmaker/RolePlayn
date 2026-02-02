b13bb8d72e292b16c3414db8b9784a8e553611f8
EM-printmaker <esashika191008@outlook.com>
2026-02-02 15:18:34 +0000

    chore: #5 フラッシュメッセージをja.ymlに移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
 
     def after_sign_in_path_for(resource)
       if resource.sign_in_count == 1 && resource.login_id.blank?
-        flash[:notice] = "ログインありがとうございます！まずはユーザーIDを設定しましょう。"
+        flash[:notice] = t("flash.sessions.welcome_and_setup_id")
         edit_user_registration_path
       else
         super
