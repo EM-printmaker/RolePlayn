8d5550b7a263b6e070f26deae1efed6e168dec1f
EM-printmaker <esashika191008@outlook.com>
2026-02-03 13:34:12 +0000

    fix: #27 controllers/index.jsの記述を調整

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,10 +2,10 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "controllers/application";
 
-import ModalController from "./modal_controller"
-application.register("modal", ModalController)
+import ModalController from "controllers/modal_controller";
+application.register("modal", ModalController);
 
-import PostAlertController from "./post_alert_controller"
-application.register("post-alert", PostAlertController)
+import PostAlertController from "controllers/post_alert_controller";
+application.register("post-alert", PostAlertController);
