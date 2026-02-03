805fc6d65182def98965e98737c0268053ae6578
EM-printmaker <esashika191008@outlook.com>
2026-02-03 17:45:23 +0000

    feat: #28 街シャッフルでのリダイレクト先をroot_pathに固定

--- app/controllers/operations/shuffles_controller.rb
+++ app/controllers/operations/shuffles_controller.rb
@@ -4,13 +4,8 @@ module Operations
 
     def create
       old_city = viewing_city
-      new_city = transition_to_city(exclude_city: old_city)
-      # root_pathにするか未定
-      if new_city
-        redirect_to city_path(new_city), status: :see_other
-      else
-        redirect_to root_path, status: :see_other
-      end
+      transition_to_city(exclude_city: old_city)
+      redirect_to root_path, status: :see_other
     end
   end
 end
