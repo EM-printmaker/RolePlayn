65da902c7d36acf6f92c231c433232b751f0b2ef
EM-printmaker <esashika191008@outlook.com>
2026-02-03 21:07:58 +0000

    chore: #26 ルーティングとSlugの指定方法を強化

--- app/models/concerns/has_slug.rb
+++ app/models/concerns/has_slug.rb
@@ -8,9 +8,12 @@ module HasSlug
     privacy profile public register search settings
     signin signout signup sitemap static stats terms
     test upload user users webhooks world worlds
+    top re_rolls shuffles character_selections rails up
+    preview expressions
   ].freeze
 
   included do
+    before_validation :normalize_slug
     validates :slug,
       presence: true,
       uniqueness: true,
@@ -23,4 +26,11 @@ module HasSlug
   def to_param
     slug
   end
+
+  private
+
+    def normalize_slug
+      return if slug.blank?
+      self.slug = slug.to_s.downcase.strip.gsub(/[^a-z0-9\-]+/, "-").gsub(/^-+|-+$/, "")
+    end
 end
