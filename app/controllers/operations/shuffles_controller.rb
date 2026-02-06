695914cab1fdf3e11b9ab9630694f2222a847a0d
EM-printmaker <esashika191008@outlook.com>
2026-02-06 09:08:18 +0000

    feat: #6 profile画面を維持したまま街のシャッフルをできるように修正

--- app/controllers/operations/shuffles_controller.rb
+++ app/controllers/operations/shuffles_controller.rb
@@ -5,7 +5,11 @@ module Operations
     def create
       old_city = viewing_city
       transition_to_city(exclude_city: old_city)
-      redirect_to root_path, status: :see_other
+      if request.referer&.include?(profile_path)
+        redirect_to profile_path, status: :see_other
+      else
+        redirect_to root_path, status: :see_other
+      end
     end
   end
 end
