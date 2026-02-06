b92a78ddc462cb00871d928eca9116365ec7367b
EM-printmaker <esashika191008@outlook.com>
2026-02-06 08:09:06 +0000

    feat: #6 ユーザーが自身の詳細ページしか見られないように設定

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   # users
-  resources :users, only: [ :show ]
+  resource :profile, only: [ :show ], controller: "users"
 
   # post
   resources :posts, only: [ :create, :destroy ]
