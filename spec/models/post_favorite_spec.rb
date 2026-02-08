0af2d6aae0c2de3baa857a1011ed49f3689d3bd0
EM-printmaker <esashika191008@outlook.com>
2026-02-08 18:57:48 +0000

    test: #6 post_favoriteとexpression_favoriteの関連付けテスト、バリデーションテストを追加

--- spec/models/post_favorite_spec.rb
+++ spec/models/post_favorite_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe PostFavorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:post_favorite) { create(:post_favorite) }
+
+  describe "associations" do
+    it_behaves_like "belongs_to_association", :user, User
+    it_behaves_like "belongs_to_association", :post, Post
+  end
+
+  describe "validations" do
+    let(:user) { create(:user) }
+    let(:post) { create(:post) }
+
+    before { create(:post_favorite, user: user, post: post) }
+
+    it "同じユーザー・同じ投稿の組み合わせは重複して登録できないこと" do
+      duplicate = build(:post_favorite,
+          user: user,
+          post: post
+        )
+      expect(duplicate).to be_invalid
+      expect(duplicate.errors[:user_id]).to include("はすでに存在します")
+    end
+  end
 end
