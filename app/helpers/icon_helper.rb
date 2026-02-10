e7e191652db6e39d6f3cd29a3f0ae4e4a7109f02
EM-printmaker <esashika191008@outlook.com>
2026-02-10 04:39:07 +0000

    refacter: #31 アイコン表示ヘルパーに共通処理のメソッドを作成

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -3,23 +3,15 @@ module IconHelper
   include SystemIconData
 
   def nav_icon(type, options = {})
-    data = NAV_DATA[type.to_sym]
-    return nil unless data
-
-    css_class= "offcanvas__icon #{options[:class]}"
-    size = options[:size] || "20"
-
-    render_base_svg(data[:path], css_class, size)
+    render_icon_from_source(NAV_DATA, type, "offcanvas__icon", "20", options)
   end
 
   def emotion_icon(type, options = {})
-    data = EMOTION_DATA[type.to_sym]
-    return nil unless data
+    render_icon_from_source(EMOTION_DATA, type, "bi", "24", options)
+  end
 
-    css_class = "bi #{options[:class]} #{data[:color]}"
-    size = options[:size] || "24"
-
-    render_base_svg(data[:path], css_class, size)
+  def system_icon(type, options = {})
+    render_icon_from_source(SYSTEM_DATA, type, "system-icon", "24", options)
   end
 
   def favorite_icon(favoritable, is_on: false, **options)
@@ -49,5 +41,20 @@ module IconHelper
         class: css_class,
         viewBox: "0 0 16 16"
       )
+    end
+
+    def render_icon_from_source(data_source, type, base_class_name, default_size, options)
+      data = data_source[type.to_sym]
+      return nil unless data
+
+      css_classes = [
+        base_class_name,
+        data[:color],
+        options[:class]
+      ].compact.join(" ")
+
+      size = options[:size] || default_size
+
+      render_base_svg(data[:path], css_classes, size)
     end
 end
